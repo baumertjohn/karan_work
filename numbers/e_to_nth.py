c95fe72d5feee90c9bd8ebf22f2fd6221a0033a3
John Baumert <baumert.john@gmail.com>
2021-07-26 21:15:01 -0600

    Cleaned up pi_to_nth - added e_to_nth

--- numbers/e_to_nth.py
+++ numbers/e_to_nth.py
@@ -1,14 +1,14 @@
-# A program to find PI to the Nth digit
-# Python PI limit is 15 digits after decimal
-import math
+# A program to generate 'e' to a given number of decimal places
+# Will probably use some code from the previous PI to the nth program.
+# Note: There are also 15 digits after the decimal when Python calculates 'e'.
 
-# print(math.pi)
+import math
 
 
 def main():
     while True:
         try:
-            digits = int(input("How many digits of PI would you like to "
+            digits = int(input("How many digits of 'e' would you like to "
                                + "display? (15 max): "))
         except ValueError:
             print("Input must be an integer between 1 and 15.")
@@ -19,8 +19,8 @@ def main():
         else:
             print("Digits must be between 1 and 15.")
 
-    pi = str(math.pi)
-    print(pi[0:digits + 2])
+    e_const = str(math.exp(1))
+    print(e_const[0:digits + 2])
 
 
 if __name__ == '__main__':
