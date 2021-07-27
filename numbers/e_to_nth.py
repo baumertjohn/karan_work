e76c3d2d1756ff31ce4a4dcfc258ce064a2e930e
John Baumert <baumert.john@gmail.com>
2021-07-27 05:06:38 -0600

    Changed PI and E_CONST to string to allow for 100 digits.

--- numbers/e_to_nth.py
+++ numbers/e_to_nth.py
@@ -1,26 +1,27 @@
 # A program to generate 'e' to a given number of decimal places
-# Will probably use some code from the previous PI to the nth program.
+# Will use code from the previous PI to the nth program.
 # Note: There are also 15 digits after the decimal when Python calculates 'e'.
+# 20210727 - Change e_const to string to allow for 100 digits
 
-import math
+E_CONST = ('2.7182818284590452353602874713526624977572470936999595749669676277'
+           '240766303535475945713821785251664274')
 
 
 def main():
     while True:
         try:
             digits = int(input("How many digits of 'e' would you like to "
-                               + "display? (15 max): "))
+                               + "display? (100 max): "))
         except ValueError:
-            print("Input must be an integer between 1 and 15.")
+            print("Input must be an integer between 1 and 100.")
             continue
 
-        if 0 < digits < 16:
+        if 0 < digits < 101:
             break
         else:
-            print("Digits must be between 1 and 15.")
+            print("Digits must be between 1 and 100.")
 
-    e_const = str(math.exp(1))
-    print(e_const[0:digits + 2])
+    print(E_CONST[0:digits + 2])  # +2 for first digit and decimal point
 
 
 if __name__ == '__main__':
--- numbers/pi_to_nth.py
+++ numbers/pi_to_nth.py
@@ -1,26 +1,26 @@
 # A program to find PI to the Nth digit
 # Python PI limit is 15 digits after decimal
-import math
+# 20210727 - Change PI to string to allow for 100 digits
 
-# print(math.pi)
+PI = ('3.141592653589793238462643383279502884197169399375105820974944592307816'
+      '4062862089986280348253421170679')
 
 
 def main():
     while True:
         try:
             digits = int(input("How many digits of PI would you like to "
-                               + "display? (15 max): "))
+                               + "display? (100 max): "))
         except ValueError:
-            print("Input must be an integer between 1 and 15.")
+            print("Input must be an integer between 1 and 100.")
             continue
 
-        if 0 < digits < 16:
+        if 0 < digits < 101:
             break
         else:
-            print("Digits must be between 1 and 15.")
+            print("Digits must be between 1 and 100.")
 
-    pi = str(math.pi)
-    print(pi[0:digits + 2])
+    print(PI[0:digits + 2])  # +2 for first digit and decimal point
 
 
 if __name__ == '__main__':
