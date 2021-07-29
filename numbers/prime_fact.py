1a2e6910f04227621700b5e9af64578c289f9c30
John Baumert <baumert.john@gmail.com>
2021-07-29 05:45:26 -0600

    Add _numbers.txt to show list of projects.

---
+++ numbers/prime_fact.py
@@ -0,0 +1,17 @@
+# Prime Factorization
+# Have the user enter a number and find all Prime Factors (if there are any)
+# and display them.
+
+# Step 1: Start by dividing the number by the first prime number 2 and continue
+# dividing by 2 until you get a decimal or remainder. Then divide by 3, 5, 7,
+# etc. until the only numbers left are prime numbers.
+
+# Step 2: Write the number as a product of prime numbers.
+# (228 = 2 * 2 * 3 * 19)
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
